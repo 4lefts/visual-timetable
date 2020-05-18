f69636a08d1d9202025ff559e9767eb5887903e0
4lefts <5t3ph3n.ball@gmail.com>
2020-05-18 20:59:52 +0100

    initial commit - simple list with add subjects working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,116 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [cards, setCards] = useState([
+    {
+      id: 0,
+      subject: "art",
+    },
+    {
+      id: 1,
+      subject: "assembly",
+    },
+    {
+      id: 2,
+      subject: "DT",
+    },
+    {
+      id: 3,
+      subject: "geography",
+    },
+    {
+      id: 4,
+      subject: "history",
+    },
+  ]);
+
+  const addCard = (newCard) => {
+    const newId = cards.length;
+    setCards([
+      ...cards,
+      {
+        id: newId,
+        subject: newCard,
+      },
+    ]);
+  };
+
+  const removeCard = (id) => {
+    console.log(id);
+    // remove the card with this id from the cards array
+
+    // reset all ids? is this necessary?
+    // should we actually use something better to set a unique id?
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Menu addCard={addCard}></Menu>
+      <Timetable cards={cards} removeCard={removeCard}></Timetable>;
+    </>
   );
-}
+};
+
+const Timetable = ({ cards, removeCard }) => (
+  <ol>
+    {cards.map((card) => (
+      <Card
+        key={card.id}
+        id={card.id}
+        subject={card.subject}
+        removeCard={removeCard}
+      ></Card>
+    ))}
+  </ol>
+);
+
+const Card = ({ id, subject, removeCard }) => {
+  const handleClick = () => {
+    removeCard(id);
+  };
+  return (
+    <li>
+      <div>{subject}</div>
+      <button onClick={handleClick}>&times;</button>
+    </li>
+  );
+};
+
+const Menu = ({ addCard }) => {
+  const subjects = [
+    "art",
+    "assembly",
+    "DT",
+    "English",
+    "geography",
+    "history",
+    "home time",
+    "languages",
+    "lunch",
+    "maths",
+    "music",
+    "PE",
+    "play time",
+    "PSHE",
+    "RE",
+    "register",
+    "reading",
+    "science",
+    "story time",
+    "test",
+  ];
+
+  const handleClick = (subject) => addCard(subject);
+
+  return (
+    <ul>
+      {subjects.map((subject) => (
+        <li key={subject}>
+          <button onClick={() => handleClick(subject)}>{subject}</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default App;
