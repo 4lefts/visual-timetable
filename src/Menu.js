15d59e6837c139bdbb12adfbd6b50fed665cf568
4lefts <5t3ph3n.ball@gmail.com>
2020-05-19 12:07:48 +0100

    added menu open close transition

--- src/Menu.js
+++ src/Menu.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { CSSTransition } from "react-transition-group";
 import styled from "styled-components";
 
 const StyledMenu = styled.div`
@@ -6,10 +7,26 @@ const StyledMenu = styled.div`
   margin: 0;
   padding: 20px;
   display: grid;
+  width: 100%;
   justify-content: center;
   grid-template-columns: repeat(auto-fit, 100px);
   grid-gap: 10px;
   background-color: white;
+  &.menu-enter {
+    position: absolute;
+    transform: translateY(-110%);
+  }
+  &.menu-enter-active {
+    transform: translateY(0);
+    transition: all 500ms ease;
+  }
+  &.menu-exit {
+    position: absolute;
+  }
+  &.menu-exit-active {
+    transform: translateY(-110%);
+    transition: all 500ms ease;
+  }
 `;
 
 const StyledButton = styled.button`
@@ -32,7 +49,15 @@ const StyledClearButton = styled(StyledButton)`
   background-color: tomato;
 `;
 
+const StyledOpenButton = styled(StyledButton)`
+  position: fixed;
+  right: 30px;
+  bottom: 30px;
+`;
+
 const Menu = ({ addCard, clearAll }) => {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
   const subjects = [
     "art",
     "assembly",
@@ -59,14 +84,26 @@ const Menu = ({ addCard, clearAll }) => {
   const handleClick = (subject) => addCard(subject);
 
   return (
-    <StyledMenu>
-      {subjects.map((subject) => (
-        <StyledButton key={subject} onClick={() => handleClick(subject)}>
-          {subject}
-        </StyledButton>
-      ))}
-      <StyledClearButton onClick={clearAll}>Clear all</StyledClearButton>
-    </StyledMenu>
+    <div>
+      <CSSTransition
+        in={menuIsOpen}
+        unmountOnExit
+        timeout={500}
+        classNames="menu"
+      >
+        <StyledMenu>
+          {subjects.map((subject) => (
+            <StyledButton key={subject} onClick={() => handleClick(subject)}>
+              {subject}
+            </StyledButton>
+          ))}
+          <StyledClearButton onClick={clearAll}>Clear all</StyledClearButton>
+        </StyledMenu>
+      </CSSTransition>
+      <StyledOpenButton onClick={() => setMenuIsOpen(!menuIsOpen)}>
+        {menuIsOpen ? "Close Menu" : "Open Menu"}
+      </StyledOpenButton>
+    </div>
   );
 };
 
